75799e74989df8931a5b0126e6362754083186e4
Bongani Ndlovu <167146765+Ndlovu19-code@users.noreply.github.com>
2024-08-24 01:13:04 +0200

    Update admin.py

--- django-models/relationship_app/admin.py
+++ django-models/relationship_app/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+# relationship_app/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Book, Library  # Import your models here
+
+# Register your models to make them accessible through the admin interface
+admin.site.register(Book)
+admin.site.register(Library)
+
