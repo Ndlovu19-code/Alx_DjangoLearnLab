c49ae323a0100e3555fbf9a965a031e0ec38f241
Bongani Ndlovu <167146765+Ndlovu19-code@users.noreply.github.com>
2024-08-24 01:14:40 +0200

    Update apps.py

--- django-models/relationship_app/apps.py
+++ django-models/relationship_app/apps.py
@@ -1,6 +1,8 @@
+# relationship_app/apps.py
+
 from django.apps import AppConfig
 
-
 class RelationshipAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'relationship_app'
+    default_auto_field = 'django.db.models.BigAutoField'  # The default type for auto-incrementing primary keys
+    name = 'relationship_app'  # The full Python path to the application
+
